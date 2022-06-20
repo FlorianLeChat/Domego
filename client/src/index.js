e1c8845d49819c5fcbf9565a3307798975ea54c9
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2022-06-20 17:08:57 +0200

    Changed the client/server interaction example

--- client/src/index.js
+++ client/src/index.js
@@ -1,165 +1,43 @@
-import React from "react";
+import { Component } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 
-function Square( props )
-{
-	return (
-		<button className="square" onClick={props.onClick}>
-			{props.value}
-		</button>
-	);
-}
-
-class Board extends React.Component
-{
-	renderSquare( i )
-	{
-		return <Square value={this.props.squares[ i ]} onClick={() => this.props.onClick( i )} />;
-	}
-
-	render()
-	{
-		return (
-			<div>
-				<div className="board-row">
-					{this.renderSquare( 0 )}
-					{this.renderSquare( 1 )}
-					{this.renderSquare( 2 )}
-				</div>
-				<div className="board-row">
-					{this.renderSquare( 3 )}
-					{this.renderSquare( 4 )}
-					{this.renderSquare( 5 )}
-				</div>
-				<div className="board-row">
-					{this.renderSquare( 6 )}
-					{this.renderSquare( 7 )}
-					{this.renderSquare( 8 )}
-				</div>
-			</div>
-		);
-	}
-}
-
-class Game extends React.Component
+class TestAPI extends Component
 {
 	constructor( props )
 	{
 		super( props );
 
-		this.state = {
-			history: [ {
-				squares: Array( 9 ).fill( null ),
-			} ],
-
-			xIsNext: true,
-			stepNumber: 0
-		};
+		this.state = { response: "" };
 	}
 
-	handleClick( i )
+	callAPI()
 	{
-		const history = this.state.history.slice( 0, this.state.stepNumber + 1 );
-		const current = history[ history.length - 1 ];
-		const squares = current.squares.slice();
-
-		if ( calculateWinner( squares ) || squares[ i ] )
-		{
-			return;
-		}
-
-		squares[ i ] = this.state.xIsNext ? "X" : "O";
-
-		this.setState( {
-			history: history.concat( [ {
-				squares: squares,
-			} ] ),
-
-			xIsNext: !this.state.xIsNext,
-			stepNumber: history.length
-		} );
+		// Requête de l'API vers le serveur NodeJS.
+		fetch( "http://localhost:3001/cats" )
+			.then( res => res.text() )
+			.then( res => this.setState( { response: res } ) )
+			.catch( err => err );
 	}
 
-	jumpTo( step )
+	componentDidMount()
 	{
-		this.setState( {
-			stepNumber: step,
-			xIsNext: ( step % 2 ) === 0,
-		} );
+		// Appel de la requête API au chargement du composant.
+		this.callAPI();
 	}
 
 	render()
 	{
-		const history = this.state.history;
-		const current = history[ this.state.stepNumber ];
-		const winner = calculateWinner( current.squares );
-
-		const moves = history.map( ( _, move ) =>
-		{
-			const desc = move ? `Revenir au tour n°${ move }` : "Revenir au début de la partie";
-
-			return (
-				<li key={move}>
-					<button onClick={() => this.jumpTo( move )}>{desc}</button>
-				</li>
-			);
-		} );
-
-		let status;
-
-		if ( winner )
-		{
-			status = `${ winner } a gagné`;
-		}
-		else
-		{
-			status = `Prochain joueur : ${ this.state.xIsNext ? "X" : "O" }`;
-		}
-
+		// Rendu final de la page.
 		return (
-			<div className="game">
-				<div className="game-board">
-					<Board squares={current.squares} onClick={( i ) => this.handleClick( i )} />
-				</div>
-				<div className="game-info">
-					<div>{status}</div>
-					<ol>{moves}</ol>
-				</div>
-			</div>
+			<section>
+				<h1>Bienvenue sur React !</h1>
+
+				<p>État de la réponse : {this.state.response}</p>
+			</section>
 		);
 	}
 }
 
-// ========================================
-
-const root = createRoot( document.getElementById( "root" ) );
-root.render( <Game /> );
-
-// ========================================
-
-function calculateWinner( squares )
-{
-	const lines = [
-		[ 0, 1, 2 ],
-		[ 3, 4, 5 ],
-		[ 6, 7, 8 ],
-		[ 0, 3, 6 ],
-		[ 1, 4, 7 ],
-		[ 2, 5, 8 ],
-		[ 0, 4, 8 ],
-		[ 2, 4, 6 ],
-	];
-
-	for ( let i = 0; i < lines.length; i++ )
-	{
-		const [ a, b, c ] = lines[ i ];
-
-		if ( squares[ a ] && squares[ a ] === squares[ b ] && squares[ a ] === squares[ c ] )
-		{
-			return squares[ a ];
-		}
-	}
-
-	return null;
-}
+const root = createRoot( document.querySelector( "main" ) );
+root.render( <TestAPI /> );
