22c36477c1c4a3c0aa63da67cdc2e738acff10b5
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2022-06-26 17:58:52 +0200

    Replaced Express to serve React static files

--- server/index.ts
+++ server/index.ts
@@ -1,11 +1,14 @@
 import cors from "cors";
+import path from "path";
 import express from "express";
 
 // Création du serveur express.
 const app = express();
 const port = 3001;
 
-app.use(cors());
+app.use( cors() );
+app.use( express.static( path.resolve( `${ __dirname }/../client/build` ) ) );
+
 app.listen( port, function ()
 {
 	console.log( `Serveur lancé sur le port ${ port }` );
@@ -17,8 +20,14 @@ import cats from "./routes/cats";
 import json from "./routes/json";
 
 app.use( "/birds", birds );
-app.use( "/cats", cats);
+app.use( "/cats", cats );
 app.use( "/json", json );
+
+// Affichage des fichiers statiques.
+app.get( "/", function ( _request, result )
+{
+	result.sendFile( path.resolve( `${ __dirname }/../client/build/index.html` ) );
+} );
 
 // Interaction quelconque avec la base de données SQL.
 app.get( "/database", function ( _request, _result )
