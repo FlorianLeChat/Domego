b8718d69c9e5303c9a7f25206bca877d466e1a66
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2022-07-02 18:14:21 +0200

    Added a router on the React side

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -5,27 +5,35 @@ import "./index.scss";
 // Importation de React et de ses composants.
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import TestApi from "./components/TestApi";
 import SocketChat from "./components/SocketChat";
 
 // Création du conteneur principal.
-export default class App extends React.Component
+export default class Home extends React.Component
 {
 	render()
 	{
 		return (
-			<main>
-				<TestApi />
-				<SocketChat />
-			</main>
+			<section>
+				<h1>Test ?</h1>
+
+				<h3>Pas un test</h3>
+			</section>
 		);
 	}
 }
 
-const root = createRoot( document.querySelector( "body > div" ) );
+const root = createRoot( document.querySelector( "body > main" ) );
 root.render(
 	<React.StrictMode>
-		<App />
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="expenses" element={<TestApi />} />
+				<Route path="invoices" element={<SocketChat />} />
+			</Routes>
+		</BrowserRouter>
 	</React.StrictMode>
 );
