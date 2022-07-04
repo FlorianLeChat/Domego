0c6384ae9da0f0af1c27e6c38d313af07fe32780
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2022-07-04 20:46:20 +0200

    Modified all NodeJS files to take care of Mongosse

---
+++ server/models/User.ts
@@ -0,0 +1,34 @@
+//
+// Modèle de données pour les utilisateurs.
+//
+import mongoose from "mongoose";
+import validator from "validator";
+
+const schema = new mongoose.Schema( {
+
+	// Adresse électronique de l'utilisateur.
+	email: {
+		type: String,
+		required: true,
+		lowercase: true,
+		validate: function ( value: any )
+		{
+			return validator.isEmail( value );
+		}
+	},
+
+	// Identité de l'utilisateur.
+	name: {
+		first: String,
+		last: String
+	},
+
+	// Âge de l'utilisateur.
+	age: Number,
+
+	// Date de création de l'utilisateur.
+	createdAt: Date
+
+} );
+
+export default mongoose.model( "User", schema );
---
+++ server/routes/users.ts
@@ -0,0 +1,76 @@
+//
+// Route fournissant des données sur les utilisateurs.
+//
+import User from "../models/User";
+import express from "express";
+
+const router = express.Router();
+
+router.route( "/" )
+
+	.get( async function ( _request, result, _next )
+	{
+		// Exemple de la méthode GET.
+		// Récupération de l'ensemble des utilisateurs (maximum 10).
+		try
+		{
+			const document = await User.find().limit( 10 );
+
+			result.json( document !== null ? { state: true, response: document } : { state: false } );
+		}
+		catch ( error )
+		{
+			result.json( { state: false } );
+		}
+	} )
+
+	.post( async function ( request, result, _next )
+	{
+		// Exemple de la méthode POST.
+		// Ajout d'un nouvel utilisateur dans la base de données.
+		try
+		{
+			const user = new User( request.body );
+			const document = await user.save();
+
+			result.json( document !== null ? { state: true, response: document } : { state: false } );
+		}
+		catch ( error )
+		{
+			result.json( { state: false } );
+		}
+	} )
+
+	.put( async function ( request, result, _next )
+	{
+		// Exemple de la méthode PUT.
+		// Modification d'un l'utilisateur dans la base de données.
+		try
+		{
+			const document = await User.findOneAndUpdate( request.body.filter, request.body.update );
+
+			result.json( document !== null ? { state: true, response: document } : { state: false } );
+		}
+		catch ( error )
+		{
+			result.json( { state: false } );
+		}
+	} )
+
+	.delete( async function ( request, result, _next )
+	{
+		// Exemple de la méthode DELETE.
+		// Suppression d'un utilisateur dans la base de données.
+		try
+		{
+			const document = await User.findOneAndDelete( request.body );
+
+			result.json( document !== null ? { state: true, response: document } : { state: false } );
+		}
+		catch ( error )
+		{
+			result.json( { state: false } );
+		}
+	} );
+
+export default router;
