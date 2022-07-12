41d92072ade77e81adb46dd7e21af0601f079541
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2022-07-12 22:33:55 +0200

    Added a new final version of the chat over sockets

--- client/src/components/SocketChat.tsx
+++ client/src/components/SocketChat.tsx
@@ -1,49 +1,112 @@
 //
 // Composant pour simuler un chat en réseau via les sockets.
 //
-import io from "socket.io-client";
-import { useEffect, useState } from "react";
+import { Component } from "react";
+import { io, Socket } from "socket.io-client";
 
 import "./SocketChat.scss";
 
-export default function SocketChat()
+interface ChatInput
 {
-	const socket = io();
-	const [ inputValue, setInputValue ] = useState( "" );
+	// Déclaration des variables de l'interface.
+	input: string;
+	socket?: Socket;
+	messages: string[];
+}
 
-	const handleInputChange = ( event: React.ChangeEvent<HTMLInputElement> ) =>
+export default class SocketChat extends Component<{}, ChatInput>
+{
+	constructor( props: ChatInput )
 	{
-		setInputValue( event.target.value );
+		// Initialisation des variables du constructeur.
+		super( props );
+
+		this.state = {
+			input: "",
+			messages: []
+		};
 	}
 
-	const handleFormSubmit = ( event: React.FormEvent<HTMLFormElement> ) =>
+	handleInputChange = ( event: React.ChangeEvent<HTMLInputElement> ) =>
 	{
+		// On récupère la valeur du champ de saisie.
+		this.setState( {
+			input: event.target.value
+		} );
+	};
+
+	handleFormSubmit = ( event: React.FormEvent<HTMLFormElement> ) =>
+	{
+		// On cesse d'abord le comportement par défaut du formulaire.
 		event.preventDefault();
 
-		socket.emit( "chat message", inputValue );
-
-		setInputValue( "" );
-	}
-
-	useEffect( () =>
-	{
-		socket.on( "chat message", ( message: string ) =>
+		// On vérifie que l'utilisateur est connecté à un socket.
+		if ( this.state.socket?.connected )
 		{
-			console.log( message );
+			// L'utilisateur est connecté, on envoie le message au serveur.
+			this.state.socket?.emit( "chat message", this.state.input );
+		}
+		else
+		{
+			// Dans le cas, on affiche un message d'erreur.
+			console.log( "Vous n'êtes pas connecté au chat." );
+		}
+
+		// On vide enfin le champ de saisie.
+		this.setState(
+			{ input: "" }
+		);
+	};
+
+	componentDidMount = () =>
+	{
+		// On crée un socket au montage du composant.
+		this.setState( { socket: io() }, () =>
+		{
+			// Une fois le socket créé, on écoute les messages du serveur.
+			this.state.socket?.on( "chat message", ( message: string ) =>
+			{
+				// Lors de chaque nouveau message, on ajoute le message en mémoire.
+				this.setState( ( state ) => ( {
+					messages: [ ...state.messages, message ]
+				} ) );
+
+				console.log( message );
+			} );
 		} );
-	} )
+	};
 
-	// Affichage du rendu HTML du composant.
-	return (
-		<section className="SocketChat">
-			<h1>Test des sockets réseau</h1>
+	componentWillUnmount = () =>
+	{
+		// On déconnecte le socket au démontage du composant.
+		this.state.socket?.disconnect();
+	};
 
-			<ul></ul>
+	render()
+	{
+		// On génère le rendu HTML du composant.
+		const messages: any = [];
 
-			<form onSubmit={handleFormSubmit}>
-				<input type="text" onChange={handleInputChange} value={inputValue} />
-				<button type="submit">Envoyer</button>
-			</form>
-		</section>
-	);
+		this.state.messages.forEach( ( element ) =>
+		{
+			messages.push( <li>{element}</li> );
+		} )
+
+		// On retourne le rendu HTML du composant.
+		return (
+			<section className="SocketChat">
+				{/* Titre de la page */}
+				<h1>Test des sockets réseau</h1>
+
+				{/* Liste des messages */}
+				<ul>{messages}</ul>
+
+				{/* Champ de saisie */}
+				<form onSubmit={this.handleFormSubmit}>
+					<input type="text" onChange={this.handleInputChange} value={this.state.input} />
+					<button type="submit">Envoyer</button>
+				</form>
+			</section>
+		);
+	}
 }
