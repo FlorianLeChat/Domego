160e4fb437da5f628d81d4cb7ab3522721a571fa
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2022-07-18 23:16:22 +0200

    Added a draft version of the game creation

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,59 +2,91 @@
 import "normalize.css";
 import "./index.scss";
 
-// Importation de React et de ses composants.
-import React from "react";
+// Importation de React et de ses dépendances.
+import { io } from "socket.io-client";
+import { Link } from "react-router-dom";
+import { randomUUID } from "crypto";
 import { createRoot } from "react-dom/client";
+import { useState, StrictMode } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+// Importation des composants React.
 import TestApi from "./components/TestApi";
 import NotFound from "./components/NotFound";
 import LiveChat from "./components/LiveChat";
 import GameRooms from "./components/GameRooms";
 
+// Création des constantes.
+const uuid = randomUUID();
+const socket = io( { path: process.env.PUBLIC_URL + "/socket.io" } );
+
 // Création du conteneur principal.
 export default function Home(): JSX.Element
 {
-	render()
+	// Déclaration des variables d'éstat.
+	const [ username, setUsername ] = useState( "" );
+
+	// Création d'une nouvelle partie.
+	const handleButtonClick = ( _event: React.MouseEvent<HTMLButtonElement> ) =>
 	{
-		return (
-			<section className="Home">
-				{/* Titre de la page */}
-				<h1>Domego</h1>
+		if ( username !== "" )
+		{
+			// Le nom d'utilisateur a été renseigné, on créé la nouvelle partie.
+			socket.emit( "joinRoom", { username, uuid } );
+		}
+		else
+		{
+			// Le nom d'utilisateur est invalide, on bloque l'action.
+			alert( "Le nom d'utilisateur est manquant ou invalide." );
+		}
+	};
 
-				{/* Descriptif du jeu */}
-				<u>Un jeu sérieux pédagogique</u>
+	// Récupération de la valeur du champ de saisie.
+	const handleInputChange = ( event: React.ChangeEvent<HTMLInputElement> ) =>
+	{
+		setUsername( event.target.value );
+	};
 
-				<article>
-					{/* Saisie d'un pseudonyme */}
-					<label>
-						Veuillez saisir un pseudonyme.
-						<input type="text" id="pseudo" placeholder="Marc007" autoComplete="off" spellCheck="false" minLength={5} maxLength={20} required />
-					</label>
+	// Affichage du rendu HTML du composant.
+	return (
+		<section className="Home">
+			{/* Titre de la page */}
+			<h1>Domego</h1>
 
-					{/* Bouton de création d'une nouvelle partie */}
-					<button type="button">Créer une partie</button>
+			{/* Descriptif du jeu */}
+			<u>Un jeu sérieux pédagogique</u>
 
-					{/* Tableau des parties en cours */}
-					<GameRooms title="Parties disponibles" />
-				</article>
-			</section>
-		);
-	}
+			<article>
+				{/* Saisie d'un pseudonyme */}
+				<label>
+					Veuillez saisir un pseudonyme.
+					<input type="text" placeholder="Marc007" autoComplete="off" spellCheck="false" minLength={5} maxLength={20} onChange={handleInputChange} value={username} />
+				</label>
+
+				{/* Bouton de création d'une nouvelle partie */}
+				<Link to={`/game/${ uuid }/${ username }`}>
+					<button type="button" onClick={handleButtonClick}>Créer une partie</button>
+				</Link>
+
+				{/* Tableau des parties en cours */}
+				<GameRooms title="Parties disponibles" />
+			</article>
+		</section>
+	);
 }
 
 const root = createRoot( document.querySelector( "body > main" ) as HTMLElement );
 root.render(
-	<React.StrictMode>
+	<StrictMode>
 		<BrowserRouter basename={process.env.PUBLIC_URL}>
 			<Routes>
 				<Route path="/">
 					<Route index element={<Home />} />
 					<Route path="*" element={<NotFound />} />
-					<Route path="chat" element={<LiveChat />} />
+					<Route path="chat" element={<LiveChat socket={socket} />} />
 					<Route path="database" element={<TestApi />} />
 				</Route>
 			</Routes>
 		</BrowserRouter>
-	</React.StrictMode>
+	</StrictMode>
 );
