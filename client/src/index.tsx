9ab75c2a06bf7703b1149cde34c575c28245ad55
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2022-07-24 14:59:18 +0200

    Fixed automatic redirection to the role selection

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -5,10 +5,9 @@ import "./index.scss";
 // Importation de React et de ses dépendances.
 import Swal from "sweetalert2";
 import { io } from "socket.io-client";
-import { Link } from "react-router-dom";
 import { createRoot } from "react-dom/client";
 import { useState, StrictMode } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
 
 // Importation des composants React.
 import TestApi from "./components/TestApi";
@@ -17,27 +16,28 @@ import LiveChat from "./components/LiveChat";
 import GameRooms from "./components/GameRooms";
 import RoleSelection from "./components/RoleSelection";
 
-// Création des constantes.
-const uuid = crypto.randomUUID();
+// Création du socket de communication.
 const socket = io( { path: process.env.PUBLIC_URL + "/socket.io" } );
 
 // Création du conteneur principal.
 export default function Home(): JSX.Element
 {
+	// Création des constantes.
+	const uuid = crypto.randomUUID();
+	const navigate = useNavigate();
+
 	// Déclaration des variables d'état.
 	const [ username, setUsername ] = useState( "" );
 
 	// Création d'une nouvelle partie.
-	const handleButtonClick = async ( event: React.MouseEvent<HTMLButtonElement> ) =>
+	const handleButtonClick = async () =>
 	{
-		// On cesse tout d'abord le comportement par défaut du formulaire.
-		event.preventDefault();
-
-		// On vérifie ensuite si l'utilisateur veut bien créer une nouvelle partie.
+		// On vérifie tout d'abord si l'utilisateur veut bien créer une nouvelle partie.
 		const result = await Swal.fire( {
 			title: "Voulez-vous créer une nouvelle partie ?",
-			icon: "warning",
+			icon: "question",
 			text: "Une fois la partie créée, vous ne pourrez plus changer de nom d'utilisateur.",
+			reverseButtons: true,
 			showCancelButton: true,
 			cancelButtonText: "Non",
 			confirmButtonText: "Oui",
@@ -47,16 +47,34 @@ export default function Home(): JSX.Element
 
 		if ( result.isConfirmed )
 		{
-			// Si c'est le cas, on vérifie si un nom d'utilisateur a bien été renseigné.
+			// Si c'est le cas, on vérifie ensuite si un nom d'utilisateur a bien été renseigné.
 			if ( username.trim() !== "" )
 			{
 				// On signale alors au serveur qu'on veut créer une nouvelle partie.
 				socket.emit( "joinRoom", username, uuid );
-				event.currentTarget.click();
+
+				// On redirige enfin l'utilisateur vers la page de sélection des rôles.
+				await Swal.fire( {
+					timer: 500,
+					title: "Création de la partie en cours...",
+					allowEscapeKey: false,
+					timerProgressBar: true,
+					allowOutsideClick: false,
+					didOpen: () =>
+					{
+						// Affichage de l'animation de chargement.
+						Swal.showLoading();
+					},
+					willClose: () =>
+					{
+						// Redirection automatique à la fin du temps d'attente.
+						navigate( `/game/${ uuid }/selection` );
+					}
+				} );
 			}
 			else
 			{
-				// Dans le cas contraire, on avertit l'utilisateur qu'une information
+				// Dans le cas contraire, on avertit enfin l'utilisateur qu'une information
 				//	est manquante.
 				Swal.fire( "Information manquante", "Le nom d'utilisateur est manquant ou invalide.", "error" );
 			}
@@ -86,9 +104,7 @@ export default function Home(): JSX.Element
 				</label>
 
 				{/* Bouton de création d'une nouvelle partie */}
-				<Link to={`/game/${ uuid }/selection`}>
-					<button type="button" onClick={handleButtonClick}>Créer une partie</button>
-				</Link>
+				<button type="button" onClick={handleButtonClick}>Créer une partie</button>
 
 				{/* Tableau des parties en cours */}
 				<GameRooms title="Parties disponibles" />
