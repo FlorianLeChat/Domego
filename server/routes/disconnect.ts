860426a85cc25da1e357c5dc4c21677b7e26784e
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2022-08-23 16:12:28 +0200

    Added a grace period before removing a disconnected user

--- server/routes/disconnect.ts
+++ server/routes/disconnect.ts
@@ -2,21 +2,42 @@
 // Fonction permettant de gérer les déconnexions des utilisateurs aux sockets.
 //
 import { updateRoom } from "../utils/RoomManager";
-import { destroyUser } from "../utils/UserManager";
 import { Server, Socket } from "socket.io";
+import { destroyUser, findUser } from "../utils/UserManager";
+
+// Temps d'attente avant une nouvelle vérification de l'identifiant
+//	unique de l'utilisateur déconnecté.
+const GRACE_TIME = 10000;
 
 export function Disconnect( io: Server, socket: Socket )
 {
-	socket.on( "disconnect", () =>
+	socket.on( "disconnect", async () =>
 	{
-		// On tente de récupérer les informations de l'utilisateur avant
-		//	de le déconnecter définitivement du salon.
-		const user = destroyUser( socket.id );
+		// On tente de récupérer les informations de l'utilisateur.
+		const user = findUser( socket.id );
 
 		if ( user )
 		{
+			// On met en mémoire son identifiant unique.
+			const identifier = user.id;
+
+			// On patiente quelques secondes afin de permettre à l'utilisateur
+			//	de se reconnecter et de reprendre la partie.
+			await new Promise( resolve => setTimeout( resolve, GRACE_TIME ) );
+
+			// On vérifie alors si l'identifiant unique a été actualisé ou non.
+			// 	Note : cela signifie que l'utilisateur s'est reconnecté.
+			if ( identifier !== socket.id )
+			{
+				return;
+			}
+
+			// Si l'utilisateur s'est déconnecté pendant une trop longue période,
+			// 	on le supprime définitivement de la liste des utilisateurs.
+			destroyUser( socket.id );
+
 			// On envoie ensuite une notification à l'ensemble des utilisateurs
-			//	encore présents dans le salon.
+			//	encore présents dans le salon afin d'indiquer son départ.
 			io.to( user.game ).emit( "GameAlert", {
 				id: user.id,
 				name: user.name,
