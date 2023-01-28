c5fb771239f7b28f934774c7877f8da3b63cd7ac
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2023-01-28 15:18:26 +0100

    Added a NextJS test redirection to the GitHub repository

--- client/next.config.js
+++ client/next.config.js
@@ -7,5 +7,15 @@ module.exports = {
 	reactStrictMode: true,
 	sassOptions: {
 		includePaths: [ path.join( __dirname, "styles" ) ],
-	}
+	},
+	async redirects()
+	{
+		return [
+			{
+				source: "/source",
+				permanent: true,
+				destination: "https://github.com/FlorianLeChat/Domego"
+			},
+		];
+	},
 };
