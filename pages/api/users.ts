de9a1ff320e4b57a1af7c123723b979eaf1576aa
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2023-02-02 15:48:33 +0100

    Updated Express route for recorded users to NextJS

---
+++ pages/api/users.ts
@@ -0,0 +1,85 @@
+//
+// Route fournissant des données sur les utilisateurs.
+//
+import User from "@/models/User";
+import { ConnectToMongoDB } from "@/utils/MongoConnection";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler( request: NextApiRequest, result: NextApiResponse )
+{
+	// Connexion à la base de données.
+	await ConnectToMongoDB();
+
+	// Détermination de la méthode HTTP utilisée.
+	switch ( request.method )
+	{
+		case "GET":
+			// Exemple de la méthode GET.
+			// Récupération de l'ensemble des utilisateurs (maximum 10).
+			try
+			{
+				const document = await User.find().limit( 10 );
+
+				result.json( document ? { state: true, response: document } : { state: false } );
+			}
+			catch ( error )
+			{
+				result.json( { state: false } );
+			}
+
+			break;
+
+		case "POST":
+			// Exemple de la méthode POST.
+			// Ajout d'un nouvel utilisateur dans la base de données.
+			try
+			{
+				const user = new User( request.body );
+				const document = await user.save();
+
+				result.json( document ? { state: true, response: document } : { state: false } );
+			}
+			catch ( error )
+			{
+				result.json( { state: false } );
+			}
+
+			break;
+
+		case "PUT":
+			// Exemple de la méthode PUT.
+			// Modification d'un l'utilisateur dans la base de données.
+			try
+			{
+				const document = await User.findOneAndUpdate( request.body.filter, request.body.update );
+
+				result.json( document ? { state: true, response: document } : { state: false } );
+			}
+			catch ( error )
+			{
+				result.json( { state: false } );
+			}
+
+			break;
+
+		case "DELETE":
+			// Exemple de la méthode DELETE.
+			// Suppression d'un utilisateur dans la base de données.
+			try
+			{
+				const document = await User.findOneAndDelete( request.body );
+
+				result.json( document ? { state: true, response: document } : { state: false } );
+			}
+			catch ( error )
+			{
+				result.json( { state: false } );
+			}
+
+			break;
+
+		default:
+			// La méthode utilisée n'est pas supportée.
+			result.status( 405 ).end();
+	}
+};
