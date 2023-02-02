a56b0e52dfba541a483d048d6ee61d733f089826
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2023-02-02 14:54:53 +0100

    Removed test routes for NextJS API

--- pages/api/rooms.ts
+++
@@ -1,6 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-
-export default function handler( _request: NextApiRequest, result: NextApiResponse )
-{
-	result.status( 200 ).json( { name: "John Doe" } );
-}
