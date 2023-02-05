d7f8d046ce4172849ad02634784da7d60e801ab0
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2023-02-05 14:31:53 +0100

    Fixed worker service registration of the offline page

--- next.config.js
+++ next.config.js
@@ -8,6 +8,14 @@ const { i18n } = require( "./next-i18next.config" );
 const withPWA = require( "next-pwa" )( {
 	dest: "public",
 	disable: process.env.NODE_ENV === "development",
+	fallbacks: {
+		// Source : https://github.com/shadowwalker/next-pwa/issues/400
+		document: process.env[ "NEXT_PUBLIC_BASE_PATH" ] + "/_offline",
+		image: "",
+		font: "",
+		audio: "",
+		video: "",
+	}
 } );
 
 module.exports = withPWA( {
