70592af99132da009121b30b276c886f51ba7d0f
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2023-02-05 15:41:16 +0100

    Improved socket reconnection on internal errors

--- utils/SocketContext.tsx
+++ utils/SocketContext.tsx
@@ -11,8 +11,9 @@ let instance!: Socket;
 if ( typeof window !== "undefined" )
 {
 	// On tente d'abord de récupérer l'identifiant unique précédemment
-	// 	mis en mémoire.
+	// 	mis en mémoire et on stocke le nombre de tentatives de connexion.
 	const cacheId = sessionStorage.getItem( "cacheId" );
+	let retries = 0;
 
 	// On crée ensuite le socket de communication avec le serveur.
 	instance = io( { path: process.env[ "NEXT_PUBLIC_BASE_PATH" ] + "/socket.io" } );
@@ -23,15 +24,30 @@ if ( typeof window !== "undefined" )
 		//	l'identifiant unique du socket.
 		sessionStorage.setItem( "cacheId", instance.id );
 	} );
-	instance.once( "connect_error", ( error ) =>
+	instance.on( "connect_error", ( error ) =>
 	{
-		// Si la connexion au serveur a échoué, on affiche enfin un message
-		// 	d'erreur à l'utilisateur.
-		console.error( error.message );
-
-		if ( Router.pathname !== "/500" )
+		// Si la connexion au serveur a échoué, on vérifie le nombre de
+		// 	tentatives de connexion.
+		if ( retries === 0 )
 		{
-			Router.push( "/500" );
+			// Si c'est la première tentative, on tente de se reconnecter
+			// 	au serveur après 1 seconde.
+			setTimeout( () =>
+			{
+				retries++;
+				instance.connect();
+			}, 1000 );
+		}
+		else
+		{
+			// Si c'est la deuxième tentative, on affiche l'erreur dans la
+			// 	console et on redirige vers la page d'erreur.
+			console.error( error.message );
+
+			if ( Router.pathname !== "/500" )
+			{
+				Router.push( "/500" );
+			}
 		}
 	} );
 }
