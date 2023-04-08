1ebe34d0eef7b4abe470e60a9c49a86f6202314a
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2023-04-08 17:51:29 +0200

    Added tests to create a new game and take its role

---
+++ tests/e2e/selection.spec.ts
@@ -0,0 +1,55 @@
+import { test, expect } from "@playwright/test";
+
+//
+// Permet de créer une nouvelle partie avant chaque test.
+//
+test.beforeEach( async ( { page } ) =>
+{
+	// Accès à la page d'accueil.
+	await page.goto( "/" );
+
+	// Remplissage d'un pseudonyme dans le champ de saisie.
+	await page.getByPlaceholder( "Marc007" ).fill( "Marc007" );
+
+	// Clic sur le bouton de création d'une nouvelle partie.
+	await page.getByRole( "button", { name: "Create a new game" } ).click();
+
+	// Clic sur le bouton pour confirmer la création d'une nouvelle partie.
+	await page.getByRole( "button", { name: "Yes" } ).click();
+
+	// Vérification du titre de la page.
+	await expect( page.getByRole( "heading", { name: "Choose your role" } ) ).toBeVisible();
+
+	// Vérification de l'URL de la page.
+	await expect( page ).toHaveURL( "/game/selection" );
+} );
+
+//
+// Permet de vérifier qu'il est possible de prendre et de confirmer un rôle.
+//
+test( "Sélection d'un rôle", async ( { page } ) =>
+{
+	// Clic sur le bouton pour prendre un rôle quelconque.
+	await page.getByLabel( "Choose this role" ).check();
+
+	// Clic sur le bouton pour confirmer la prise du rôle.
+	await page.getByLabel( "I'm ready!" ).check();
+
+	// Vérification de la confirmation de la prise du rôle.
+	await expect( page.getByText( "Marc007", { exact: true } ) ).toBeVisible();
+} );
+
+//
+// Permet de vérifier qu'il est possible de parler dans le chat.
+//
+test( "Utilisation du chat", async ( { page } ) =>
+{
+	// Remplissage d'un message dans le champ de saisie.
+	await page.getByPlaceholder( "Lorem ipsum dolor sit amet..." ).fill( "Bonjour" );
+
+	// Clic sur le bouton pour envoyer le message.
+	await page.getByRole( "button", { name: "Send" } ).click();
+
+	// Vérification de l'envoi du message.
+	await expect( page.getByText( "Bonjour" ).first() ).toBeVisible();
+} );
