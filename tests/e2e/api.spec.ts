c5720550649eeabc7c8162c790fb9a605fdc4282
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2023-04-08 17:46:18 +0200

    Added PlayWright tests to MongoDB test API

---
+++ tests/e2e/api.spec.ts
@@ -0,0 +1,59 @@
+import { test, expect } from "@playwright/test";
+
+//
+// Permet de vérifier les requêtes de type GET à l'API MongoDB.
+//
+test( "Requête de type GET à l'API", async ( { request } ) =>
+{
+	const response = await request.get( "/api/users" );
+
+	expect( response.ok() ).toBeTruthy();
+} );
+
+//
+// Permet de vérifier les requêtes de type POST à l'API MongoDB.
+//
+test( "Requête de type POST à l'API", async ( { request } ) =>
+{
+	const response = await request.post( "/api/users", {
+		params: {
+			email: "florian@gmail.com",
+			name: "{\"first\":\"Florian\",\"last\":\"Trayon\"}",
+			age: Math.floor( Math.random() * 100 ) + 1
+		}
+	} );
+
+	expect( response.ok() ).toBeTruthy();
+} );
+
+//
+// Permet de vérifier les requêtes de type PUT à l'API MongoDB.
+//
+test( "Requête de type PUT à l'API", async ( { request } ) =>
+{
+	const response = await request.put( "/api/users", {
+		params: {
+			email: "florian@gmail.com",
+			name: "{\"filter\":{\"email\":\"florian@gmail.com\",\"age\":{\"$not\":{\"$eq\":10}}},\"update\":{\"age\":10}}",
+			age: Math.floor( Math.random() * 100 ) + 1
+		}
+	} );
+
+	expect( response.ok() ).toBeTruthy();
+} );
+
+//
+// Permet de vérifier les requêtes de type DELETE à l'API MongoDB.
+//
+test( "Requête de type DELETE à l'API", async ( { request } ) =>
+{
+	const response = await request.delete( "/api/users", {
+		params: {
+			email: "florian@gmail.com",
+			name: "{\"age\":10}",
+			age: Math.floor( Math.random() * 100 ) + 1
+		}
+	} );
+
+	expect( response.ok() ).toBeTruthy();
+} );
