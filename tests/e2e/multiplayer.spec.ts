7b256845e5766ef1b59889b5f8f5a0133c4ac9b9
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2023-04-14 12:03:42 +0200

    Fixed PlayWright unit tests of the multiplayer area

--- tests/e2e/multiplayer.spec.ts
+++ tests/e2e/multiplayer.spec.ts
@@ -5,8 +5,6 @@ import { test, expect } from "@playwright/test";
 //
 test( "Connexion à une partie existante", async ( { browser } ) =>
 {
-	test.skip();
-
 	// Création de deux contextes de navigateur.
 	const player1Context = await browser.newContext();
 	const player2Context = await browser.newContext();
@@ -22,6 +20,7 @@ test( "Connexion à une partie existante", async ( { browser } ) =>
 	await player1Page.getByPlaceholder( "Marc007" ).fill( "Player 1" );
 	await player1Page.getByRole( "button", { name: "Create a new game" } ).click();
 	await player1Page.getByRole( "button", { name: "Yes" } ).click();
+	await player1Page.waitForLoadState( "networkidle" );
 
 	// Choix du rôle pour le joueur 1.
 	await expect( player1Page ).toHaveURL( "/game/selection" );
@@ -29,15 +28,16 @@ test( "Connexion à une partie existante", async ( { browser } ) =>
 
 	// Connexion à la partie par le joueur 2.
 	await player2Page.getByPlaceholder( "Marc007" ).fill( "Player 2" );
-	await player2Page.getByRole( "button", { name: "Join" } ).first().click();
-	await player2Page.getByRole( "button", { name: "Yes" } ).first().click();
+	await player2Page.getByRole( "button", { name: "Join" } ).last().click();
+	await player2Page.getByRole( "button", { name: "Yes" } ).click();
+	await player2Page.waitForLoadState( "networkidle" );
 
 	// Vérification du rôle du joueur 1 par le joueur 2.
 	await expect( player2Page ).toHaveURL( "/game/selection" );
-	await expect( player2Page.getByText( "Player 1" ) ).toBeVisible();
+	await expect( player2Page.getByText( "Player 1" ).first() ).toBeVisible();
 
 	// Choix du rôle pour le joueur 2.
-	await player2Page.getByText( "Choose this role" ).nth( 1 ).check();
+	await player2Page.getByText( "Choose this role" ).last().check();
 
 	// Vérification du rôle du joueur 2 par le joueur 1.
 	await expect( player1Page.getByText( "Player 2" ).first() ).toBeVisible();
@@ -64,17 +64,19 @@ test( "Pseudonyme déjà utilisé", async ( { browser } ) =>
 	await player1Page.getByPlaceholder( "Marc007" ).fill( "Player 1" );
 	await player1Page.getByRole( "button", { name: "Create a new game" } ).click();
 	await player1Page.getByRole( "button", { name: "Yes" } ).click();
+	await player1Page.waitForLoadState( "networkidle" );
 
 	await expect( player1Page ).toHaveURL( "/game/selection" );
 
 	// Connexion à la partie par le joueur 2.
 	await player2Page.getByPlaceholder( "Marc007" ).fill( "Player 1" );
-	await player2Page.getByRole( "button", { name: "Join" } ).first().click();
-	await player2Page.getByRole( "button", { name: "Yes" } ).first().click();
+	await player2Page.getByRole( "button", { name: "Join" } ).last().click();
+	await player2Page.getByRole( "button", { name: "Yes" } ).click();
+	await player2Page.waitForLoadState( "networkidle" );
 
 	// Vérification du message d'erreur par le serveur.
 	await expect( player2Page ).toHaveURL( "/" );
-	await expect( player2Page.getByRole( "heading", { name: "Duplicated username" } ) ).toBeVisible();
+	await expect( player2Page.getByRole( "dialog", { name: "Duplicated username" } ) ).toBeVisible();
 } );
 
 //
@@ -90,20 +92,23 @@ test( "Partie encore active", async ( { page } ) =>
 	await page.getByPlaceholder( "Marc007" ).fill( "Player 1" );
 	await page.getByRole( "button", { name: "Create a new game" } ).click();
 	await page.getByRole( "button", { name: "Yes" } ).click();
+	await page.waitForLoadState( "networkidle" );
 
 	await expect( page ).toHaveURL( "/game/selection" );
 
 	// Retour à la page d'accueil.
 	await page.goBack();
+	await page.waitForLoadState( "networkidle" );
 
 	// Création d'une nouvelle partie (encore).
 	await page.getByPlaceholder( "Marc007" ).fill( "Player 1" );
 	await page.getByRole( "button", { name: "Create a new game" } ).click();
 	await page.getByRole( "button", { name: "Yes" } ).click();
+	await page.waitForLoadState( "networkidle" );
 
 	// Vérification du message d'erreur par le serveur.
 	await expect( page ).toHaveURL( "/" );
-	await expect( page.getByRole( "heading", { name: "Data duplication" } ) ).toBeVisible();
+	await expect( page.getByRole( "dialog", { name: "Data duplication" } ) ).toBeVisible();
 } );
 
 //
@@ -112,8 +117,6 @@ test( "Partie encore active", async ( { page } ) =>
 //
 test( "Reconnexion à la volée", async ( { page } ) =>
 {
-	test.skip();
-
 	// Accès à la page d'accueil.
 	await page.goto( "/" );
 
@@ -121,16 +124,19 @@ test( "Reconnexion à la volée", async ( { page } ) =>
 	await page.getByPlaceholder( "Marc007" ).fill( "Player 1" );
 	await page.getByRole( "button", { name: "Create a new game" } ).click();
 	await page.getByRole( "button", { name: "Yes" } ).click();
+	await page.waitForLoadState( "networkidle" );
 
 	await expect( page ).toHaveURL( "/game/selection" );
 
 	// Retour à la page d'accueil.
 	await page.goBack();
+	await page.waitForLoadState( "networkidle" );
 
 	// Reconnexion à la partie.
 	await page.getByPlaceholder( "Marc007" ).fill( "Player 1" );
-	await page.getByRole( "button", { name: "Join" } ).first().click();
-	await page.getByRole( "button", { name: "Yes" } ).first().click();
+	await page.getByRole( "button", { name: "Join" } ).last().click();
+	await page.getByRole( "button", { name: "Yes" } ).click();
+	await page.waitForLoadState( "networkidle" );
 
 	// Vérification de la redirection vers la page de sélection de rôle.
 	await expect( page ).toHaveURL( "/game/selection" );
