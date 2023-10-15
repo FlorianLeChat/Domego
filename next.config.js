f2f487d4048234e63d2ce7557b86d7ef7e5a4c12
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2023-10-15 21:28:55 +0200

    Removed deprecated dependency handling PWA for NextJS

--- next.config.js
+++ next.config.js
@@ -1,27 +1,18 @@
 // @ts-check
 
-const path = require( "path" );
-const basePath = new URL( process.env.NEXT_PUBLIC_URL ?? "" ).pathname;
-
 /**
  * @type {import("next").NextConfig}
  */
-const { i18n } = require( "./next-i18next.config" );
-const withPWA = require( "next-pwa" )( {
-	dest: "public",
-	disable: process.env.NODE_ENV === "development",
-	fallbacks: {
-		// Source : https://github.com/shadowwalker/next-pwa/issues/400
-		document: path.join( basePath, "_offline" ),
-		image: "",
-		font: "",
-		audio: "",
-		video: ""
-	}
-} );
+const { i18n } = require("./next-i18next.config");
+const basePath = new URL(process.env.NEXT_PUBLIC_URL ?? "").pathname;
 
-module.exports = withPWA( {
+module.exports = {
 	i18n,
-	basePath: basePath === "/" ? "" : ( basePath.endsWith( "/" ) ? basePath.slice( 0, -1 ) : basePath ),
-	poweredByHeader: false
-} );
+	basePath:
+		basePath === "/"
+			? ""
+			: basePath.endsWith("/")
+			? basePath.slice(0, -1)
+			: basePath,
+	poweredByHeader: false,
+};
