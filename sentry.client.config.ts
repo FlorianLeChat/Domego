6e386da658fbe5d94c11079864a5a65628aae8d0
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2023-10-16 09:27:18 +0200

    Fixed NextJS configuration based on Sentry activation state

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -1,19 +1,16 @@
 import { init, Replay } from "@sentry/nextjs";
 
-if ( process.env.SENTRY_ENABLED === "true" )
-{
-	init( {
-		dsn: process.env.SENTRY_DSN,
-		debug: false,
-		integrations: [
-			new Replay( {
-				maskAllText: true,
-				blockAllMedia: true
-			} )
-		],
-		tracesSampleRate: 1,
-		replaysOnErrorSampleRate: 1.0,
-		replaysSessionSampleRate:
-			process.env.NODE_ENV === "development" ? 1.0 : 0.1
-	} );
-}
+init( {
+	dsn: process.env.SENTRY_DSN,
+	debug: false,
+	integrations: [
+		new Replay( {
+			maskAllText: true,
+			blockAllMedia: true
+		} )
+	],
+	tracesSampleRate: 1,
+	replaysOnErrorSampleRate: 1.0,
+	replaysSessionSampleRate:
+		process.env.NODE_ENV === "development" ? 1.0 : 0.1
+} );
--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -1,10 +1,7 @@
 import { init } from "@sentry/nextjs";
 
-if ( process.env.SENTRY_ENABLED === "true" )
-{
-	init( {
-		dsn: process.env.SENTRY_DSN,
-		debug: false,
-		tracesSampleRate: 1
-	} );
-}
+init( {
+	dsn: process.env.SENTRY_DSN,
+	debug: false,
+	tracesSampleRate: 1
+} );
